a4ea7671db8a46f7549cd7d85cd3ec7282130c5e
Gunnar Jul Jensen <gjj@envidan.dk>
2021-10-02 18:08:50 +0200

    Code lint

--- Skatteberegner/Beregning.cs
+++ Skatteberegner/Beregning.cs
@@ -8,16 +8,18 @@ namespace Skatteberegner
 {
     public class Beregning
     {
-        // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
+        /// <summary>
+        /// Beregning af skatpligtig beløb
+        /// </summary>
+        /// <param name="julegave">Værdi af julegaver</param>
+        /// <param name="andenGave">Værdi andre gaver</param>
+        /// <returns></returns>
         public double SkatVedJulegave(double julegave, double andenGave)
         {
             
             if (andenGave + julegave <= 1200)
                 return 0;
-            //Overstiger det samlede beloeb 1200 kr skal alt beskattes
-
-            //Selvom det samlede beløb overstiger 1200 bliver julegaven ikke beskattet hvis dem koster 900 eller under
-
+   
             if (julegave <= 900)
                 return andenGave;
 
